26ebf0fc3a432f37ffd061cb5c3bf094c30c84fa
Tushar Sharma <ts17995@gmail.com>
2019-09-21 01:55:00 +0530

    initial commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+function getURL() {
+  if (process.env.NODE_ENV === "production") {
+    return "mongodb://localhost:27017/real_db";
+  } else {
+    return "mongodb://localhost:27017/test_db";
+  }
+}
+
+module.exports = getURL;
